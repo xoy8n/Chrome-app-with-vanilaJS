8d969488778a49d61266150c2ba63d964b4769a8
soyoon <87157589+xoy8n@users.noreply.github.com>
2021-12-15 16:32:18 +0900

    #7.7 Deleting To Dos part two

--- js/todo.js
+++ js/todo.js
@@ -55,14 +55,42 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos); //string형이 array로 변함
   toDos = parsedToDos;
+  // foreach는 paintTodo를 기본적으로 실행함, foreach는 각각의 item(object)를 줌
   parsedToDos.forEach(paintToDo);
 }
 
-/* 
-    parsedToDos.forEach((item) => console.log("this is the turn of", item));
-    이렇게 쓰거나 밑처럼 쓰거나 똑같음
-    function sayHello(item) {
-    console.log("this is the turn of", item);
-    }
- 
- */
+// array에서 item을 삭제하고 싶으면 filter를 이용해서 지우고 싶은 item을 제외하고 새 array를 만든다.
+function sexyFilter() {
+  // 이 함수는 만약 새 array에도 기존 아이템들을 포함시키고 싶다면 반드시 true를 리턴해야 함
+  // 만약 false를 리턴한다면 기존 item들은 새 array에 포함되지 않는다.
+  // !== 는 다르면 남아있고, 같으면 array에서 없어진다.
+}
+
+/*
+const arr =[1234,3535,2465,464,234356,234]
+function sexyFilter(number){return number > 1000}
+
+arr.filter(sexyFilter) // [1234, 3535, 2465, 234356]
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+
+const todos = [{"text":"hello","id":1639552017017},{"text":"a","id":1639552112384},{"text":"b","id":1639552112986},{"text":"c","id":1639552113541}]
+function sexyFilter(todo){return todo.id!== 1639552017017}
+
+todos.filter(sexyFilter) // 같은(1639552017017)id를 제외하고 3개가 array에 남아있음
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+
+const todos = [{text:"lalala"}, {text:"lololo"}]
+function sexyFilter(todo){return todo.text !=="lololo"}
+
+todos.filter(sexyFilter) // [{text:"lalala"}]
+*/
