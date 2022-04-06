8e9dca9d2cc40d9f28c485fed14824d14f4957f1
xoy8n <s01057111917@gmail.com>
2022-04-06 16:23:13 +0900

    #11.8 background animation

--- js/background.js
+++ js/background.js
@@ -4,6 +4,8 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
+const quotebox = document.querySelector(".quote");
+
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document.quotebox.appendChild(bgImage);
